006a9e530c4c667ec73a844b7f53ebbc3c239cc6
RomainLefranc <lefranc46@gmail.com>
2021-08-27 18:38:25 +0400

    Initial commit

---
+++ src/services/auth-header.js
@@ -0,0 +1,10 @@
+export default function authHeader() {
+  // recuperation du token
+  let user = JSON.parse(localStorage.getItem("user"));
+
+  if (user && user.accessToken) {
+    return { Authorization: "Bearer " + user.accessToken };
+  } else {
+    return {};
+  }
+}
